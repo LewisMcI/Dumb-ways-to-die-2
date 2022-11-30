07d4f6e328405fd7f041b5c5eb3867e9a3b1e3ce
ektorzoidis <zoidisektor@gmail.com>
2022-11-30 20:15:45 +0000

    Updated fridge trap.

--- Unity Game Folder/Assets/Scripts/Interactables/TrapFridge.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/TrapFridge.cs
@@ -5,30 +5,29 @@ using UnityEngine;
 
 public class TrapFridge : Interactable
 {
+    #region fields
+    [SerializeField]
+    private Rigidbody tableRig;
+    #endregion
+
     #region methods
     public override void Action()
     {
         // Ignore fridge objects
         gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
         transform.GetChild(1).GetChild(0).gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-        RaycastHit hit;
+
         // Cast ray to see if player will be hit
-        //if (Physics.Raycast(transform.GetChild(0).GetChild(0).transform.position, Vector3.right, out hit, 2f))
-        //if (Physics.BoxCast(transform.GetChild(0).GetChild(0).transform.position, Vector3.right, out hit, 2f))
+        RaycastHit hit;
         if (Physics.BoxCast(transform.GetChild(0).GetChild(0).transform.position, new Vector3(0.5f, 0.2f, 0.6f), Vector3.right, out hit, Quaternion.identity, 3f))
         {
-            Debug.Log(hit.transform.tag);
-            Debug.Log(hit.transform.name);
             if (hit.transform.tag == "Player" || hit.transform.tag == "MainCamera")
                 StartCoroutine(TriggerTrap());
             else
                 GetComponent<Animator>().SetTrigger("Activate");
         }
         else
-        {
-            Debug.Log("asd");
             GetComponent<Animator>().SetTrigger("Activate");
-        }
     }
 
     IEnumerator TriggerTrap()
@@ -36,6 +35,7 @@ public class TrapFridge : Interactable
         float delay = 0.75f;
         GameManager.Instance.Player.GetComponent<PlayerController>().Die(PlayerController.SelectCam.fridgeCam, delay);
         yield return new WaitForSeconds(delay);
+        tableRig.isKinematic = false;
         GetComponent<Animator>().SetTrigger("Activate");
     }
     #endregion
