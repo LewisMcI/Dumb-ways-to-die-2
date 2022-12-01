29e814fc9037ca08ed5aae14c6c582a2c2dca6b0
ektorzoidis <zoidisektor@gmail.com>
2022-12-01 16:10:45 +0000

    Completed couch.

--- Unity Game Folder/Assets/Scripts/Interactables/TrapCouch.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/TrapCouch.cs
@@ -5,50 +5,95 @@ using UnityEngine;
 public class TrapCouch : Interactable
 {
     #region fields
-    private bool sat;
+    [SerializeField]
+    private bool spring;
+
+    private Vector3 originalPos;
+    private Quaternion originalRot;
+    private bool transition, sitting;
     #endregion
 
     #region methods
     private void Update()
     {
-        if (sat)
+        // Trigger get up
+        if (sitting && Input.GetButtonDown("Interact"))
+        {
+            StartCoroutine(UnsetSitting());
+        }
+
+        if (transition)
         {
             float speed = 4.0f;
-            // Change player orientation
-            PlayerController.Instance.transform.rotation = Quaternion.Lerp(PlayerController.Instance.transform.rotation, transform.rotation, speed * Time.deltaTime);
-            // Change player position
-            PlayerController.Instance.transform.position = Vector3.Lerp(PlayerController.Instance.transform.position, transform.GetChild(0).position, speed * Time.deltaTime);
-            // Disable collision
-            PlayerController.Instance.GetComponent<Rigidbody>().isKinematic = true;
+            // Sit down transition
+            if (!sitting)
+            {
+                // Change player orientation
+                PlayerController.Instance.transform.rotation = Quaternion.Lerp(PlayerController.Instance.transform.rotation, transform.rotation, speed * Time.deltaTime);
+                // Change player position
+                PlayerController.Instance.transform.position = Vector3.Lerp(PlayerController.Instance.transform.position, transform.GetChild(0).position, speed * Time.deltaTime);
+                // Disable collision
+                PlayerController.Instance.GetComponent<Rigidbody>().isKinematic = true;
+            }
+            // Get up transition
+            else
+            {
+                // Reset
+                PlayerController.Instance.transform.rotation = Quaternion.Lerp(PlayerController.Instance.transform.rotation, originalRot, speed * Time.deltaTime);
+                PlayerController.Instance.transform.position = Vector3.Lerp(PlayerController.Instance.transform.position, originalPos, speed * Time.deltaTime);
+                PlayerController.Instance.GetComponent<Rigidbody>().isKinematic = false;
+                PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().SetBool("Sit", false);
+                GameManager.Instance.EnableControls = true;
+                GameManager.Instance.EnableCamera = true;
+            }
         }
     }
 
     public override void Action()
     {
-        sat = true;
-
         // Disable controls
         GameManager.Instance.EnableControls = false;
+        GameManager.Instance.EnableCamera = false;
         PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().SetBool("Sit", true);
-        StartCoroutine(TriggerTrap());
+        // Save transform
+        originalPos = PlayerController.Instance.transform.position;
+        originalRot = PlayerController.Instance.transform.rotation;
+
+        if (spring)
+            StartCoroutine(TriggerTrap());
+        else
+            StartCoroutine(SetSitting());
     }
 
     IEnumerator TriggerTrap()
     {
+        transition = true;
         float delay = 1f;
         PlayerController.Instance.GetComponent<PlayerController>().Die(PlayerController.SelectCam.couchCam, delay);
         yield return new WaitForSeconds(delay);
+        transition = false;
         // Add upward force
         PlayerController.Instance.AddRagdollForce(new Vector3(0, 100, 0));
         // Enable collision
         PlayerController.Instance.GetComponent<Rigidbody>().isKinematic = false;
         // Play animation
-        transform.parent.GetComponent<Animator>().SetTrigger("Activate");
+        transform.parent.parent.GetComponent<Animator>().SetTrigger("Activate");
+    }
 
-        Debug.Log(GameManager.Instance.EnableControls);
-        Debug.Log(PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().GetBool("Sit"));
+    IEnumerator SetSitting()
+    {
+        transition = true;
+        yield return new WaitForSeconds(1.0f);
+        transition = false;
+        sitting = true;
+    }
 
-        sat = false;
+    IEnumerator UnsetSitting()
+    {
+        transition = true;
+        yield return new WaitForSeconds(1.0f);
+        transition = false;
+        sitting = false;
     }
     #endregion
 }
