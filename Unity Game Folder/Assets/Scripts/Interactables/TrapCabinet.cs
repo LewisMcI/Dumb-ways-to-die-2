d4dc4826b35b6c4c8d67228be786797cbada96fa
broanite <lmcint218@caledonian.ac.uk>
2022-12-06 01:35:50 +0000

    Little changed to TV

--- Unity Game Folder/Assets/Scripts/Interactables/TrapCabinet.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/TrapCabinet.cs
@@ -8,6 +8,8 @@ public class TrapCabinet : Interactable
     private bool cut;
 
     private Animator anim;
+
+    public bool kills = false;
     #endregion
 
     #region methods
@@ -38,10 +40,45 @@ public class TrapCabinet : Interactable
             // Open and shoot if not cut
             if (cut)
                 anim.SetTrigger("Open");
-            else
+            else if (!cut && kills)
+            {
+                // Cast ray to see if player will be hit
+                RaycastHit hit;
                 anim.SetTrigger("Shoot");
+                if (Physics.BoxCast(transform.GetChild(0).GetChild(0).transform.position, new Vector3(0.5f, 0.2f, 0.6f), Vector3.right, out hit, Quaternion.identity, 3f))
+                {
+                    if (hit.transform.tag == "Player" || hit.transform.tag == "MainCamera")
+                        StartCoroutine(TriggerTrap());
+                    else
+                        GetComponent<Animator>().SetTrigger("Activate");
+                }
+                else
+                    GetComponent<Animator>().SetTrigger("Activate");
+            }
+            else
+            {
+                anim.SetTrigger("Shoot");
+            }
+
             GetComponent<Collider>().enabled = false;
         }
+    }
+
+    IEnumerator TriggerTrap()
+    {
+        // Disable controls
+        GameManager.Instance.EnableControls = false;
+        // Reset player velocity and animation
+        PlayerController.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().SetFloat("dirX", 0);
+        PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().SetFloat("dirY", 0);
+        float delay = 0.75f;
+        PlayerController.Instance.Die(PlayerController.SelectCam.bathroomCam, delay);
+        yield return new WaitForSeconds(delay);
+        // Add backwards force
+        PlayerController.Instance.AddRagdollForce(new Vector3(100, 10, 0));
+        GetComponent<Animator>().SetTrigger("Activate");
+        GetComponent<AudioSource>().Play();
     }
     #endregion
 }
--- Unity Game Folder/Assets/Scripts/Player/PlayerController.cs
+++ Unity Game Folder/Assets/Scripts/Player/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     private bool isCrouching, isJumping;
 
     [SerializeField]
-    private Camera toasterCam, fridgeCam, couchCam;
+    private Camera toasterCam, fridgeCam, couchCam, bathroomCam;
 
     private float restartTimer = 5f;
     private bool dead;
@@ -214,6 +214,9 @@ public class PlayerController : MonoBehaviour
             case SelectCam.couchCam:
                 couchCam.enabled = true;
                 break;
+            case SelectCam.bathroomCam:
+                bathroomCam.enabled = true;
+                break;
         }
         dead = true;
         Debug.Log(dead);
@@ -239,7 +242,8 @@ public class PlayerController : MonoBehaviour
     {
         toasterCam,
         fridgeCam,
-        couchCam
+        couchCam,
+        bathroomCam
     }
     #endregion
 }
