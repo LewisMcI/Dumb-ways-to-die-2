972c0159c5d4ff699d537f679500a56aa0d990cc
ektorzoidis <zoidisektor@gmail.com>
2022-12-11 14:07:29 +0000

    Added shotgun shoot effect and fixes.

--- Unity Game Folder/Assets/Scripts/Interactables/TrapCabinet.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/TrapCabinet.cs
@@ -44,20 +44,16 @@ public class TrapCabinet : Interactable
             {
                 // Cast ray to see if player will be hit
                 RaycastHit hit;
-                anim.SetTrigger("Shoot");
                 if (Physics.BoxCast(transform.GetChild(0).GetChild(0).transform.position, new Vector3(0.5f, 0.2f, 0.6f), Vector3.right, out hit, Quaternion.identity, 3f))
                 {
                     if (hit.transform.tag == "Player" || hit.transform.tag == "MainCamera")
                         StartCoroutine(TriggerTrap());
-                    else
-                        GetComponent<Animator>().SetTrigger("Activate");
                 }
-                else
-                    GetComponent<Animator>().SetTrigger("Activate");
+                anim.SetTrigger("Shoot Smoke");
             }
-            else
+            else if (!cut && !kills)
             {
-                anim.SetTrigger("Shoot");
+                anim.SetTrigger("Shoot Confetti");
             }
 
             GetComponent<Collider>().enabled = false;
@@ -72,12 +68,11 @@ public class TrapCabinet : Interactable
         PlayerController.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
         PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().SetFloat("dirX", 0);
         PlayerController.Instance.transform.GetChild(0).GetComponent<Animator>().SetFloat("dirY", 0);
-        float delay = 0.75f;
+        float delay = 0.5f;
         PlayerController.Instance.Die(PlayerController.SelectCam.bathroomCam, delay);
         yield return new WaitForSeconds(delay);
         // Add backwards force
         PlayerController.Instance.AddRagdollForce(new Vector3(100, 10, 0));
-        GetComponent<Animator>().SetTrigger("Activate");
         GetComponent<AudioSource>().Play();
     }
     #endregion
