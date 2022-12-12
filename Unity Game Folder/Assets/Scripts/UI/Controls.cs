49dc5bec4682c303e97e9aa3ccf820c3efc070a6
ektorzoidis <zoidisektor@gmail.com>
2022-12-12 19:31:51 +0000

    Loading screen for controls.

--- Unity Game Folder/Assets/Scripts/UI/Controls.cs
+++ Unity Game Folder/Assets/Scripts/UI/Controls.cs
@@ -6,9 +6,20 @@ using UnityEngine.UI;
 
 public class Controls : MonoBehaviour
 {
+    #region fields
+    [SerializeField]
+    private GameObject loading;
+    #endregion
+
+    #region methods
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !loading.activeSelf)
+        {
+            gameObject.SetActive(false);
+            loading.SetActive(true);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
+    #endregion
 }
