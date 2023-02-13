bc8453e7b101c79823c1a51bd32839eb57514b76
broanite <lmcint218@caledonian.ac.uk>
2023-02-13 09:53:57 +0000

    Bubble Effect & Interactivity Changes

--- Unity Game Folder/Assets/Scripts/Interactables/ToothBrush.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/ToothBrush.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ToothBrush : Interactable
 {
+    VisualEffect bubbleVFX;
     #region methods
+    private void Awake()
+    {
+        bubbleVFX = GameObject.Find("Player Bubble Effect").GetComponent<VisualEffect>();
+        if (!bubbleVFX)
+            Debug.Log("Could not find player bubble effect");
+    }
     public override void Action()
     {
         AudioManager.Instance.PlayAudio("Brush Teeth");
+        bubbleVFX.Play();
         CanInteract = false;
 
         GameManager.Instance.taskManager.UpdateTaskCompletion("Brush Teeth");
