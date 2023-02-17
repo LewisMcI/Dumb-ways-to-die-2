df69d57cddab18fa9b2b56fb2cb43df4d899dcb7
broanite <lmcint218@caledonian.ac.uk>
2023-02-17 22:27:30 +0000

    Small Scene Edits

--- Unity Game Folder/Assets/Scripts/Interactables/LightSwitch.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/LightSwitch.cs
@@ -8,20 +8,17 @@ public class LightSwitch : Interactable
     #region fields
     [SerializeField]
     private GameObject[] lights;
-    [SerializeField]
-    bool active;
+
     #endregion
 
     #region methods
     public override void Action()
     {
-        // Switch
-        active = !active;
         // Set lights
         foreach (GameObject light in lights)
-            light.SetActive(active);
+            light.SetActive(!light.activeSelf);
         // Switch scale
-        transform.parent.localScale = (active) ? new Vector3(1.0f, -1.0f, 1.0f) : Vector3.one;
+        transform.parent.localScale = (lights[0].activeSelf) ? new Vector3(1.0f, -1.0f, 1.0f) : Vector3.one;
 
         // Player audio
         AudioManager.Instance.PlayAudio("Switch");
