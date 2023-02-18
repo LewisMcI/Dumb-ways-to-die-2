db70f026b0e690820a38e12bbe749b8ed075dff5
ektorzoidis <zoidisektor@gmail.com>
2023-02-18 14:23:19 +0000

    Light switches fixed.

--- Unity Game Folder/Assets/Scripts/Interactables/LightSwitch.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/LightSwitch.cs
@@ -8,15 +8,32 @@ public class LightSwitch : Interactable
     #region fields
     [SerializeField]
     private GameObject[] lights;
+    [SerializeField]
+    private MeshRenderer[] bulbs;
+    [SerializeField]
+    private Material emissionOn, emissionOff;
 
+    private bool isOn = true;
     #endregion
 
     #region methods
+    private void Awake()
+    {
+        Action();
+    }
+
     public override void Action()
     {
+        // Switch
+        isOn = !isOn;
+
         // Set lights
         foreach (GameObject light in lights)
-            light.SetActive(!light.activeSelf);
+            light.SetActive(isOn);
+        // Set bulbs
+        foreach (MeshRenderer bulb in bulbs)
+            bulb.material = (isOn) ? emissionOn : emissionOff;
+
         // Switch scale
         transform.parent.localScale = (lights[0].activeSelf) ? new Vector3(1.0f, -1.0f, 1.0f) : Vector3.one;
 
