d6b419743eba270c0cb10e85add3d179dbac18d8
ektorzoidis <zoidisektor@gmail.com>
2023-02-19 21:13:06 +0000

    Phone ring.

--- Unity Game Folder/Assets/Scripts/Interactables/Phone.cs
+++ Unity Game Folder/Assets/Scripts/Interactables/Phone.cs
@@ -2,13 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Phone : MonoBehaviour
+public class Phone : Interactable
 {
-    #region fields
-
-    #endregion
-
     #region methods
+    private void Awake()
+    {
+        StartCoroutine(StartAlarm());
+    }
 
+    public override void Action()
+    {
+        GetComponent<DialogueSystem>().TriggerDialogue();
+        // Stop audio
+        GetComponent<AudioSource>().Stop();
+        // Stop animation
+        GetComponent<Animator>().SetBool("Ring", false);
+        CanInteract = false;
+    }
+
+    public void Ring()
+    {
+        // Play audio
+        GetComponent<AudioSource>().Play();
+        // Play animation
+        GetComponent<Animator>().SetBool("Ring", true);
+    }
+
+    private IEnumerator StartAlarm()
+    {
+        yield return new WaitForSeconds(0.25f);
+        Ring();
+    }
     #endregion
 }
