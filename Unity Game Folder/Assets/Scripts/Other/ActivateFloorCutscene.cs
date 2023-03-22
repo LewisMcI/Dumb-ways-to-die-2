a3cc1153185ee69f10b65babea7b0f857efc6041
ektorzoidis <zoidisektor@gmail.com>
2023-03-22 22:49:51 +0000

    cutscene in.

---
+++ Unity Game Folder/Assets/Scripts/Other/ActivateFloorCutscene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ActivateFloorCutscene : MonoBehaviour
+{
+    #region fields
+    private VideoPlayer video;
+    #endregion
+
+    #region methods
+    private void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            GameManager.Instance.EnableControls = false;
+            GameManager.Instance.EnableCamera = false;
+            video.enabled = true;
+            StartCoroutine(Switch());
+        }
+    }
+
+    IEnumerator Switch()
+    {
+        yield return new WaitForSeconds(13.0f);
+        GameManager.Instance.MoveToNextLevel();
+    }
+    #endregion
+}
