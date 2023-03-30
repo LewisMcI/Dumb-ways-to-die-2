99ff2f99ecc2f9f573c1063a0af543e78da276a5
broanite <lmcint218@caledonian.ac.uk>
2023-03-30 07:16:00 +0100

    Basketball Fix

--- Unity Game Folder/Assets/BasketBallHoop.cs
+++ Unity Game Folder/Assets/BasketBallHoop.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class BasketBallHoop : MonoBehaviour
 {
+    [SerializeField]
+    VisualEffect vfx;
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Basketball")
         {
-            Destroy(gameObject);
+            vfx.Play();
         }
     }
 }
