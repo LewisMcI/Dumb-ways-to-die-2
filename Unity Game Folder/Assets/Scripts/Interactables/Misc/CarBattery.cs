72b19cf63c8326b07da57110feb32f596ae84850
ektorzoidis <zoidisektor@gmail.com>
2023-04-03 11:51:40 +0100

    moved interactable scripts to folders.

---
+++ Unity Game Folder/Assets/Scripts/Interactables/Misc/CarBattery.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBattery : Interactable
+{
+    #region fields
+    [SerializeField]
+    private Clothes clothes;
+    #endregion
+
+    #region methods
+    public override void Action()
+    {
+        // Disable electricity
+        foreach (Clothing clothing in clothes.Clothings)
+        {
+            clothing.Electric = false;
+        }
+        // Stop sfx
+        transform.parent.GetComponent<AudioSource>().Stop();
+        // Play animation
+        transform.parent.GetComponent<Animator>().SetTrigger("Trigger");
+        // Play switch sfx
+        AudioManager.Instance.PlayAudio("Switch");
+    }
+    #endregion
+}
