4b86e6b444d044d6a98867bd261606cfe70b0a5c
broanite <lmcint218@caledonian.ac.uk>
2023-04-04 12:20:03 +0100

    Recorder now swaps music

--- Unity Game Folder/Assets/Scripts/PlayMusic.cs
+++ Unity Game Folder/Assets/Scripts/PlayMusic.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class PlayMusic : Interactable
 {
+    [SerializeField]
+    AudioClip[] audioClips;
     AudioSource audioSource;
+
+    int index = 0;
+
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClips[index];
+        audioSource.Play();
     }
     public override void Action()
     {
-        if (!audioSource.isPlaying)
+        if (audioClips.Length == 0) return;
+        if (audioClips.Length == 1) 
+        {
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+            else
+                audioSource.Stop();
+        }
+        else
+        {
+            index++;
+            if (index >= audioClips.Length) index = 0;
+            audioSource.clip = audioClips[index];
             audioSource.Play();
-        else 
-            audioSource.Stop();
+        }
     }
 }
